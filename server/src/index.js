fd8958ffd9c964670fd08f7877cc2959de253657
miracoly <68049792+miracoly@users.noreply.github.com>
2024-06-16 15:56:28 +0200

    start server

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,14 @@
-//TODO
+import { ApolloServer } from "@apollo/server";
+import { typeDefs } from "./schema.js";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const startApolloServer = async () => {
+  const server = new ApolloServer({ typeDefs });
+  const { url } = await startStandaloneServer(server);
+  console.log(`
+    🚀  Server is running!
+    📭  Query at ${url}
+  `);
+};
+
+await startApolloServer();
