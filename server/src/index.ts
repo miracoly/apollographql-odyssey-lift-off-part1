65b5360165b6efb065d8504cbf7a1e735725c3cf
miracoly <68049792+miracoly@users.noreply.github.com>
2024-06-16 16:39:55 +0200

    add custom mocks

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,11 +3,13 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 import { addMocksToSchema } from "@graphql-tools/mock";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { typeDefs } from "./schema";
+import { mocks } from "./mocks";
 
 const startApolloServer = async () => {
   const server = new ApolloServer({
     schema: addMocksToSchema({
       schema: makeExecutableSchema({ typeDefs }),
+      mocks,
     }),
   });
   const { url } = await startStandaloneServer(server);
